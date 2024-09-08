88c3bad175f46941904c4555090b1f907d1ee606
cgp-client <code.gen.pro.system@gmail.com>
2024-09-08 08:29:10 +0300

    Adding middlewares/auth-middleware.ts

---
+++ middlewares/auth-middleware.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import { verifyToken as vt } from "../utils/auth-utils";
+
+
+export function verifyTokenMW(req: Request, res: Response, next: NextFunction) {
+    try {          
+        const token = req.header("Authorization")?.substring(7) || "";                         
+        const user = vt(token);
+        res.locals.user = user;                
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+export function verifyTokenAdminMW(req: Request, res: Response, next: NextFunction) {
+    try {          
+        const token = req.header("Authorization")?.substring(7) as string;
+        vt(token, true);
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
